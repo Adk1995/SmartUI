8540a26f526957f8f2050dca918da04a077e83d7
Adhokshith <adatta9@uic.edu>
2018-07-16 15:13:13 -0500

    UI, css

---
+++ js/openForms.js
@@ -0,0 +1,33 @@
+
+function showDemographics(){
+  let toggleDemo=document.getElementById('demographics');
+  if(toggleDemo.style.display==='none')
+  {
+    toggleDemo.style.display = 'block';
+  }
+  else{
+    toggleDemo.style.display='none';
+  }
+}
+
+function showCancerDescriptors(){
+  let toggleDemo=document.getElementById('cancerDescriptors');
+  if(toggleDemo.style.display==='none')
+  {
+    toggleDemo.style.display = 'block';
+  }
+  else{
+    toggleDemo.style.display='none';
+  }
+}
+
+function showTreatment(){
+  let toggleDemo=document.getElementById('treatment');
+  if(toggleDemo.style.display==='none')
+  {
+    toggleDemo.style.display = 'block';
+  }
+  else{
+    toggleDemo.style.display='none';
+  }
+}
